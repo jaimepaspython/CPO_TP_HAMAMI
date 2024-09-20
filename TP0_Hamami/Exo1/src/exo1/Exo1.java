aa78d7997a56e9e555225a449dcb1fdd3c572d1b
Raphael <Raphael@Monpc>
2024-09-20 17:49:44 +0200

    exo 1 tp0

---
+++ TP0_Hamami/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,36 @@
+package exo1;
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+ /* TP0 Exercice 1
+ *  Septembre 2024 TDB
+ * Raphael
+ */
+
+/**
+ *
+ * @author Raphael
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom s'il vous plait ?");
+        prenom = sc.nextLine();
+    }
+    
+    
+}
+
